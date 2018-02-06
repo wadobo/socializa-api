fc87223053252a08fa566767991796a2c0726848
Victor Ramirez <virako@wadobo.com>
2018-02-06 08:05:34 +0100

    Removed django_extensions dependency

--- socializa/settings.py
+++ socializa/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.gis',
-    'django_extensions',
 
     'django_nose',
     'rest_framework',
