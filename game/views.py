7e1da8433dcf005474b9b5821a687c1c870088c9
rarellano <rarellano@wadobo.com>
2019-01-07 13:08:39 +0100

    Implements game status for a player distance

--- game/views.py
+++ game/views.py
@@ -1,11 +1,19 @@
+from django.db.models import Q
 from django_filters.rest_framework import DjangoFilterBackend
+from django.contrib.gis.measure import D
 from django.core.exceptions import FieldError
-from rest_framework import generics
+from rest_framework import generics, views
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, SAFE_METHODS
+from rest_framework.response import Response
+
 
 from owner.models import Owner
 from .models import Game
 from .serializers import GameSerializer
+from character.models import Player
+from contents.serializers import ContentSerializer
+from contents.models import Content
+from django.contrib.contenttypes.models import ContentType
 
 
 class GameListCreate(generics.ListCreateAPIView):
@@ -45,3 +53,31 @@ class GameDetail(generics.RetrieveUpdateDestroyAPIView):
             player = self.request.user.character_player
             games = Game.objects.filter(owners__in=player.owners.all())
         return games
+
+
+class GameStatusForPlayer(views.APIView):
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
+    def get(self, request, version, pk, *args, **kwargs):
+        self.player = Player.objects.get(user=request.user)
+        self.player_content = Content.objects.get(content_type=ContentType.objects.get(pk=17),
+                                                     content_id=self.player.pk)
+        game = Game.objects.get(pk=pk)
+        contents = Content.objects.filter(game=game)
+
+        q = Q(position__distance_lte=(self.player_content.position, D(m=game.preferences.vision_distance)))
+
+        result = {
+                    'npcs': [ContentSerializer(x).data
+                             for x in contents.filter(content_type=ContentType.objects.get(pk=16))
+                                              .filter(q)],
+                    'players': [ContentSerializer(x).data
+                                for x in contents.filter(content_type=ContentType.objects.get(pk=17))
+                                                 .filter(q)
+                                                 .exclude(pk=self.player_content.pk)],
+                    'items': [ContentSerializer(x).data
+                              for x in contents.filter(content_type=ContentType.objects.get(pk=22))
+                                               .filter(q)]
+                    }
+
+        return Response(result)
