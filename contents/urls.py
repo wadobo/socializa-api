fe153a1bbb9c45c6f2a969ccebabd7b5970ebf83
rarellano <rarellano@wadobo.com>
2019-01-08 12:05:06 +0100

    Implemented: ContentTypes API call

--- contents/urls.py
+++ contents/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('', views.ContentListCreate.as_view(), name="content_list"),
     path('<int:pk>/', views.ContentDetail.as_view(), name="content_detail"),
+    path('types/', views.ContentTypes.as_view(), name="content_types"),
 ]
