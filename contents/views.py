043a29571eae8f12df2765d762a1f97d9b99146f
Rafael Arellano <rarellano@users.noreply.github.com>
2019-01-11 11:43:45 +0100

    Add API call for get all ContentTypes
    This API call return a python dict with ContentTypes used for save contents. The dict is composed by key: ContentType pk and value: ContentType name
    
    Resolves: #24

--- contents/views.py
+++ contents/views.py
@@ -1,7 +1,9 @@
 from django.core.exceptions import FieldError
+from django.contrib.contenttypes.models import ContentType
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import generics
+from rest_framework import generics, views
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from game.models import Game
 from .models import Content
@@ -33,3 +35,14 @@ class ContentDetail(generics.RetrieveUpdateDestroyAPIView):
         player = self.request.user.character_player
         games = Game.objects.filter(owners__in=player.owners.all())
         return Content.objects.filter(game__in=games)
+
+
+class ContentTypes(views.APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request, version, *args, **kwargs):
+        content_types_list = ('player', 'npc', 'knowledge', 'item', 'rol')
+        content_types = ContentType.objects.filter(
+            model__in=content_types_list)
+        result = {c.pk: c.model for c in content_types}
+        return Response(result)
